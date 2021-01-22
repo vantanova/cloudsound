e8f89468d5791421343bf0422c41676c9e3d8950
Antonio <anguilar@aol.com>
2021-01-22 10:03:54 -0800

    Added validations

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,5 +1,23 @@
 // backend/routes/api/users.js
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const asyncHandler = require("express-async-handler");
+const { User } = require("../../db/models");
 const express = require("express");
 const router = express.Router();
+
+// Sign up
+router.post(
+  "",
+  asyncHandler(async (req, res) => {
+    const { email, password, username } = req.body;
+    const user = await User.signup({ email, username, password });
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user,
+    });
+  })
+);
 
 module.exports = router;

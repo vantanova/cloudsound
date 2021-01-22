e3c11a2ac0e44edd1d8db792bc08df737a540fe5
Antonio <anguilar@aol.com>
2021-01-22 10:13:52 -0800

    Finished backend part 1

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -4,13 +4,25 @@ const router = express.Router();
 const asyncHandler = require("express-async-handler");
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
 
+const validateLogin = [
+  check("credential")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid email or username."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a password."),
+  handleValidationErrors,
+];
 // backend/routes/api/session.js
-// ...
 
 // Log in
 router.post(
   "/",
+  validateLogin,
   asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 

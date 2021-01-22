acdd3b1822cacdcf14d2c7e417fe05a01b1c2d55
Antonio <anguilar@aol.com>
2021-01-22 09:43:33 -0800

    Finished route testing

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,6 +7,23 @@ router.get("/hello/world", function (req, res) {
   res.send("Hello World!");
 });
 
+// GET /api/set-token-cookie
+const asyncHandler = require("express-async-handler");
+const { setTokenCookie } = require("../utils/auth");
+const { User } = require("../db/models");
+router.get(
+  "/set-token-cookie",
+  asyncHandler(async (req, res) => {
+    const user = await User.findOne({
+      where: {
+        username: "Demo-lition",
+      },
+    });
+    setTokenCookie(res, user);
+    return res.json({ user });
+  })
+);
+
 module.exports = router;
 
 // API routes

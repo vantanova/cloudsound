59bcc55ee79d563547558bafee8eb027a0a6e098
Antonio <anguilar@aol.com>
2021-01-22 09:57:43 -0800

    Tested log in

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,13 @@
 // backend/routes/api/index.js
 const router = require("express").Router();
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
+
+router.use("/session", sessionRouter);
+
+router.use("/users", usersRouter);
+
+module.exports = router;
 
 router.post("/test", function (req, res) {
   res.json({ requestBody: req.body });

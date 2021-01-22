061f07533e5af87cd5c4d9b2218f00e918356bcf
Antonio <anguilar@aol.com>
2021-01-22 10:46:45 -0800

    Express added to react

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider as ReduxProvider } from 'react-redux';
-import './index.css';
-import App from './App';
-import configureStore from './store';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import configureStore from "./store";
+import { restoreCSRF, fetch } from "./store/csrf";
 
+// ... const store = configureStore();
 const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  restoreCSRF();
+
+  window.csrfFetch = fetch;
+  window.store = store;
+}
 
 if (process.env.NODE_ENV !== "production") {
   window.store = store;
@@ -26,5 +35,5 @@ ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );

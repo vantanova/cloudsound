bc8c8a0c9238d644ea7b041c05b90b1aff8f105d
Antonio <anguilar@aol.com>
2021-01-26 21:49:45 -0800

    Styling

--- frontend/src/components/ProfilePage/index.js
+++ frontend/src/components/ProfilePage/index.js
@@ -2,16 +2,17 @@ import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { fetch } from "../../store/csrf";
-import ProfileHeader from "./header";
-import "antd/dist/antd.css";
-import { Layout } from "antd";
+import { Layout, Row, Col, Upload, message, Input, Image } from "antd";
 import "./Header.css";
-const { Header, Footer, Sider, Content } = Layout;
+import "antd/dist/antd.css";
 
 function ProfilePage() {
+  const { Header, Footer, Sider, Content } = Layout;
   let history = useHistory();
   const [data, setData] = useState();
   const sessionUser = useSelector((state) => state.session.user);
+  let username;
+  let profileData;
 
   const userId = sessionUser.id;
 
@@ -23,17 +24,78 @@ function ProfilePage() {
     }
   }
 
+  if (data) {
+    username = data.userFiles[0].username;
+    profileData = data.userFiles[0].Profile;
+    if (profileData.profilePicture === "default") {
+      profileData.profilePicture =
+        "https://cloudsoundappbucket.s3-us-west-1.amazonaws.com/logo.png";
+    }
+    console.log(profileData);
+  }
+
   useEffect(() => {
     profileFetch();
   }, []);
 
   return (
-    <Layout style={{ background: "none" }}>
-      {/* <ProfileHeader data={data}></ProfileHeader> */}
-      <Layout style={{ background: "none" }}>
-        <Content style={{ backgroundColor: "#edeeef", marginTop: "0px" }}>
-          <div>
-            <p>Hello</p>
+    <Layout style={{ background: "none", padding: "none" }}>
+      <Layout
+        style={{
+          background: "none",
+          padding: "none",
+        }}
+      >
+        <Content
+          style={{
+            backgroundColor: "#edeeef",
+            marginTop: "0px",
+            maxWidth: "800px",
+          }}
+        >
+          <div
+            className="header"
+            style={{
+              backgroundImage: `url("https://cloudsoundappbucket.s3-us-west-1.amazonaws.com/Default-Banner.png")`,
+              backgroundSize: "cover",
+              position: "relative",
+              zIndex: "1",
+            }}
+          >
+            {data ? (
+              <Image
+                style={{
+                  margin: "25px",
+                  zIndex: "2",
+                  position: "absolute",
+                }}
+                width={"25%"}
+                src={profileData.profilePicture}
+                loading="true"
+                preview="true"
+                placeholder="true"
+              ></Image>
+            ) : (
+              <Image
+                style={{
+                  margin: "25px",
+                  zIndex: "2",
+                  position: "absolute",
+                }}
+                width={"25%"}
+                src={
+                  "https://cloudsoundappbucket.s3-us-west-1.amazonaws.com/logo.png"
+                }
+                placeholder="true"
+              ></Image>
+            )}
+
+            <Row>
+              <Col span={8}></Col>
+              <Col span={4}>
+                <p> Hello </p>
+              </Col>
+            </Row>
           </div>
         </Content>
       </Layout>
